3b764ab16e81c39e30a9c582b08ecf124eac0a13
arjunnsarkarr <arjunsarkar8756@gmail.com>
2024-04-06 01:04:33 +0530

    testing deployment

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const ReviewList = () => {
   return (
     <>
-      <div className="text-3xl font-bold underline text-purple-400">ReviewList</div>
-      
+      <div className="text-3xl font-bold underline text-purple-400">ReviewList
+      <h1>made by papa</h1></div>
+
     </>
   );
 };
